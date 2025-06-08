98e689eae34ca9f851ef6b7c8811497eb8b92523
Saniya2229 <saniyahakim22@gmail.com>
2025-06-08 13:05:20 +0530

    Add files via upload

---
+++ 06_num_and_math.js
@@ -0,0 +1,33 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.6666
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreded = 1000000
+// console.log(hundreded.toLocaleString('en-IN'));
+
+// ************************* MAths ********************
+
+console.log(Math);
+
+// console.log(Math.abs(-5)); //onlt neagative values are converted into positive
+// console.log(Math.round(4.7)); 
+
+// console.log(Math.ceil(4.8)); 
+// console.log(Math.floor(4.3)); 
+
+console.log(Math.random()); 
+console.log((Math.random()*10) + 1); 
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random () * (max - min + 1)) + min)
